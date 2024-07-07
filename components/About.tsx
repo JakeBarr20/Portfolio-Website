109997d61477fe10947c3b56404700dd7d37e326
JakeBarr20 <jakebiddibarr@hotmail.com>
2024-07-07 22:27:34 +0100

    added scroller colouring

--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,10 @@
+import Scroller from "./Scroller";
+
 const About: React.FC = () => {
     return (
         <div>
             <h1>Jake Biddiscombe-Barr</h1>
-
+            <Scroller></Scroller>
         </div>
     )
 }
---
+++ components/Scroller.tsx
@@ -0,0 +1,17 @@
+const Scroller: React.FC = () => {
+    return (
+        <div className="scroller">
+            <div className="point"></div>
+            <hr></hr>
+            <div className="point"></div>
+            <hr></hr>
+            <div className="point"></div>
+            <hr></hr>
+            <div className="point"></div>
+            <hr></hr>
+            <div className="point"></div>
+        </div>
+    )
+}
+
+export default Scroller;
